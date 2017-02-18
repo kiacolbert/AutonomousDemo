85e489d87e593686530cb0fc12221eb5bb1e9ab7
Unknown <colber16@gmail.com>
2017-02-18 07:09:29 -0500

    Because I need to commit
    Not sure what just happened

---
+++ AutonomousDemo/Speedometer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutonomousDemo
+{
+    public class Speedometer
+    {
+        public void SpeedometerDisplay(VehicleMotion vehicleMotion, int travelTime, Position position)
+        {
+
+            var metersPerSecond= vehicleMotion.PressGasPedal(travelTime, position);
+            var milesPerHour = CalculateMilesPerHour(metersPerSecond);
+            Console.WriteLine($"Speed: {milesPerHour}");
+        }
+        public double ConvertMetersPerSecondToMilesPerHour(double metersPerSecond)
+        {
+            return metersPerSecond * 2.2369;
+        }
+        
+    }
+}
